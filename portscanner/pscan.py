e6f93d5de881ad9b6b3cee9d2aafd5db3fe0d256
dalfior <aecio.dalfior@gmail.com>
2021-08-29 20:11:50 -0300

    Lab Dio Portscanner - Parte 03

--- portscanner/pscan.py
+++ portscanner/pscan.py
@@ -1,7 +1,20 @@
 import socket
 
-client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+ip = input("Digite o host ou IP: ")
+ports = []
+count = 0
 
-code = client.connect_ex(("google.com", 80))
-
-print(code)
+while count < 3:
+	ports.append(int(input("Digite a porta: ")))
+	count += 1
+	
+for port in ports:
+	client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+	client.settimeout(0.05)
+	code = client.connect_ex((ip, port))
+	
+	if code == 0:
+		print(str(port), " -> Porta aberta.")
+	
+	else:
+		print(str(port), " -> Porta fechada.")
